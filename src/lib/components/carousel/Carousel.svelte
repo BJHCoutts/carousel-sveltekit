ed2d0bd3200f4fa585a2bee976c149cc98062fc9
Brian Coutts <bjhcoutts@gmail.com>
2023-09-21 21:55:20 -0700

    starting initial file structure

---
+++ src/lib/components/carousel/Carousel.svelte
@@ -0,0 +1,33 @@
+<script lang='ts'>
+	import type { slide } from "./types";
+
+	export let slides:slide[] = []
+
+</script>
+
+<div class='carousel-container'>
+
+	<div class='carousel-track'>
+		<ul>
+			{#each slides as slide}
+				<li></li>
+			{/each}
+		</ul>
+	</div>
+
+	<ul>
+		{#each {length: totalDots} as _, i}
+			<li class/>
+		{/each}
+	</ul>
+
+</div>
+
+<style>
+
+	.carousel-container {
+		position: relative;
+		width: 100%;
+	}
+
+</style>
---
+++ src/lib/components/carousel/types.ts
@@ -0,0 +1,5 @@
+export type slide = {
+	bgimage: string,
+	title: string,
+	description: string,
+}
