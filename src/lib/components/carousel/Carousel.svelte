a1d605002096fe42c0013bc492e002e444c9c6cf
Brian Coutts <bjhcoutts@gmail.com>
2023-09-24 15:17:38 -0700

    checkpoint before siema test

--- src/lib/components/carousel/Carousel.svelte
+++ src/lib/components/carousel/Carousel.svelte
@@ -1,26 +1,33 @@
 <script lang='ts'>
-
+	
+	import Siema from 'siema'
 	import CardBaked from "./CardBaked.svelte";
 	import type { card } from "./types";
 
 	export let cardsData:[]|card[] = []
 
+	if (cardsData.length) {
+		console.log('lets go')
+	}
+
 </script>
 
 <div class='carousel-container'>
 
-		<ul class='carousel-track'>
-			{#if cardsData.length}
-				{#each cardsData as cardData}
-					<li>
-						<CardBaked {cardData} />
-					</li>
-				{/each}
-			{:else}
-					<p>There are no card to display</p>
-			{/if}
-		</ul>
+	<ul class='carousel-track siema'>
+		{#if cardsData.length}
+			{#each cardsData as cardData}
+				<li>
+					<CardBaked {cardData} />
+				</li>
+			{/each}
+		{:else}
+				<p>There are no card to display</p>
+		{/if}
+	</ul>
 
+	<span class='control' >Left</span>
+	<span class='control' >Right</span>
 
 	<ul>
 		<!-- {#each {length: totalDots} as _, i}
@@ -39,8 +46,13 @@
 
 	.carousel-track {
 		list-style: none;
-		display: grid;
-		grid-gap: 2.5em;
+		position: relative;
+		width: 100%;
+	}
+
+	.control {
+		display: inline-block;
+		margin-top: 6rem;
 	}
 
 </style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,22 @@
+<h1>Welcome to SvelteKit</h1>
+
+<div class="siema">
+	<div class='slide'></div>
+	<div class='slide'></div>
+	<div class='slide'></div>
+</div>
+
+<button class="prev">prev</button>
+<button class="next">next</button>
+
+
 <script lang='ts'>
+	import { onMount } from 'svelte';
+
 
 	import Carousel from '../lib/components/carousel/Carousel.svelte';
 	import type { card } from '../lib/components/carousel/types.js';
+	import Siema from 'siema';
 
 	export let data
 
@@ -11,8 +26,31 @@
 		cardsData = data.cardsData
 	}
 
+	onMount( 
+		() => {
+		
+		// document.querySelector('.prev').addEventListener('click', () => mySiema.prev());
+		// document.querySelector('.next').addEventListener('click', () => mySiema.next());
+		
+		console.log('test')
+		console.log(Siema)
+		const mySiema = new Siema()
+			// console.log(mySiema)
+		}
+	)
+
+
 </script>
 
-<h1>Welcome to SvelteKit</h1>
 
-<Carousel {cardsData} />
+
+
+<style>
+	.slide{
+		background:blue;
+		height: 3rem;
+		width: 3rem;
+		}
+</style>
+
+<!-- <Carousel {cardsData} /> -->
